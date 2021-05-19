06f08cee85713bd8bd35d83a4d28c8f427e2c55f
chrisoney <christopher.oney@yahoo.com>
2021-05-18 20:18:12 -0700

    answer form styling

--- react-app/src/components/Answer/answer_form.js
+++ react-app/src/components/Answer/answer_form.js
@@ -64,55 +64,57 @@ const AnswerForm = ({error_id}) => {
   }
 
   return (
-    <form className={styles.answer_form} onSubmit={onSubmit}>
-      <h3 className={styles.answer_form_header}>
-        Submit an answer for this error
-      </h3>
-      <div>
-        {errors.map((error, idx) => (
-          <div className={styles.error} key={idx}>{error}</div>
-        ))}
-      </div>
-      <div className={styles.answer_form_input_section}>
-        <textarea
-          className={styles.description_input}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <label
-        htmlFor="upload-box"
-        className="upload-label-box"
-        onDrop={(e) => handleDrop(e)}
-        onDragOver={e => handleDragOver(e)}
-        onDragEnter={e => handleDragEnter(e)}
-        >
-          <div className={styles.upload_container}>
-            <div className={`${styles.camera_icon} fas fa-camera`} />
-            <div className={styles.image_label}>Drag and drop or click here to select image files</div>
-          </div>
-          <input
-            className={styles.upload_input}
-            type="file"
-            accept="image/*"
-            id="upload-box"
-            value=""
-            onChange={updateImages}
-            multiple
+    <div className={styles.answer_form_container}>
+      <form className={styles.answer_form} onSubmit={onSubmit}>
+        <h3 className={styles.answer_form_header}>
+          Submit an answer for this error
+        </h3>
+        <div>
+          {errors.map((error, idx) => (
+            <div className={styles.error} key={idx}>{error}</div>
+          ))}
+        </div>
+        <div className={styles.answer_form_input_section}>
+          <textarea
+            className={styles.description_input}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
-        </label>
-      <div className={styles.button_container}>
-        <button className={styles.form_button} type="submit">
-          Submit
-        </button>
-      </div>
-      </div>
-      {(imageLoading) && <p className={styles.loading}>Loading...</p>}
-      <div className={styles.answer_form_img_previews}>
-        {imageUrls.map((image, idx) => {
-          return <img key={idx} alt="" className={styles.preview_image} src={image}/>
-        })}
-      </div>
-    </form>
+          <label
+          htmlFor="upload-box"
+          className={styles.upload_label_box}
+          onDrop={(e) => handleDrop(e)}
+          onDragOver={e => handleDragOver(e)}
+          onDragEnter={e => handleDragEnter(e)}
+          >
+            <div className={styles.upload_container}>
+              <div className={`${styles.camera_icon} fas fa-camera`} />
+              <div className={styles.image_label}>Drag and drop or click here to select image files</div>
+            </div>
+            <input
+              className={styles.upload_input}
+              type="file"
+              accept="image/*"
+              id="upload-box"
+              value=""
+              onChange={updateImages}
+              multiple
+            />
+          </label>
+        </div>
+        <div className={styles.button_container}>
+          <button className={styles.form_button} type="submit">
+            Submit
+          </button>
+        </div>
+        {(imageLoading) && <p className={styles.loading}>Loading...</p>}
+        <div className={styles.answer_form_img_previews}>
+          {imageUrls.map((image, idx) => {
+            return <img key={idx} alt="" className={styles.preview_image} src={image}/>
+          })}
+        </div>
+      </form>
+    </div>
   )
 }
 
